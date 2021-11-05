2314930c848c380a1f43097714384023b09493b5
David Südholt <david.suedholt@gmail.com>
2021-11-05 14:34:59 +0200

    Construct checkpoint path from model args in compress_and_train.py

--- compress_and_train.py
+++ compress_and_train.py
@@ -1,3 +1,4 @@
+import os
 from models import LstmModel
 from pytorch_lightning import Trainer
 from argparse import ArgumentParser
@@ -8,14 +9,29 @@ from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
 
 def compress_and_train(args):
+    dev = args.device_name
+    hs = args.hidden_size
+    prune_pct = args.prune_pct
+    prune_iter = args.prune_iter
+
+    base_dir = f'{dev}-LSTM{hs}-prune{prune_pct}'
+
+    run_dir = os.path.join(base_dir, str(prune_iter), 'lightning_logs')
+    run_dir = os.path.join(run_dir, os.listdir(run_dir)[0])
+
+    ckpt_path = os.path.join(run_dir, 'checkpoints')
+    ckpt_path = os.path.join(ckpt_path, os.listdir(ckpt_path)[0])
+
     data = AudioDataModule.from_argparse_args(args)
 
     callbacks = [
         ModelCheckpoint(monitor='val_loss', save_top_k=1, save_last=True),
-        EarlyStopping(monitor='val_loss')
+        EarlyStopping(monitor='val_loss', patience=args.early_stopping_patience)
     ]
-    trainer = Trainer(dirpath=args.dirpath, gpus=1, check_val_every_n_epoch=2, enable_progress_bar=False, num_sanity_val_steps=0, callbacks=callbacks)
-    model = LstmModel.load_from_checkpoint(args.ckpt)
+
+    dir_path = os.path.join(base_dir, str(prune_iter), 'completed')
+    trainer = Trainer(default_root_dir=dir_path, gpus=1, check_val_every_n_epoch=2, enable_progress_bar=False, num_sanity_val_steps=0, callbacks=callbacks)
+    model = LstmModel.load_from_checkpoint(ckpt_path)
     model.compress()
 
     trainer.fit(model, data)
@@ -23,11 +39,11 @@ def compress_and_train(args):
 
 
 if __name__ == "__main__":
-    # todo: construct ckpt_path from device, architecture, prune_pct, prune_iter
     parser = ArgumentParser()
     AudioDataModule.add_argparse_args(parser)
-    parser.add_argument("--ckpt", type=str)
+    parser.add_argument("--prune_pct", type=int, default=30)
+    parser.add_argument("--prune_iter", type=int, default=0)
+    parser.add_argument("--hidden_size", type=int, default=64)
     parser.add_argument("--early_stopping_patience", type=int, default=25)
-    parser.add_argument("--dirpath", type=str, default=".")
 
     compress_and_train(parser.parse_args())
--- mask_search.py
+++ mask_search.py
@@ -11,6 +11,11 @@ from argparse import ArgumentParser
 
 
 class MaskSearchEarlyStopping(EarlyStopping):
+
+    # Implements the "early-bird ticket" stopping criterion
+    # https://arxiv.org/abs/1909.11957
+    # Stop training once the distance between successive pruning masks falls below some threshold
+
     def __init__(self, pl_module, prune_amount, k_masks=5, **kwargs):
         super().__init__(**kwargs)
         self.k_masks = k_masks
@@ -65,7 +70,7 @@ class MaskSearchEarlyStopping(EarlyStopping):
 
 
 def mask_search(args):
-    basepath = (
+    base_path = (
         f"{args.device_name}-LSTM{args.hidden_size}-prune{int(args.prune_amount * 100)}"
     )
     data = AudioDataModule.from_argparse_args(args)
@@ -74,7 +79,7 @@ def mask_search(args):
     meta_logs = {"compression": [], "val_loss": []}
 
     for i in range(args.prune_iters):
-        dirpath = os.path.join(basepath, str(i))
+        dir_path = os.path.join(base_path, str(i))
 
         ckpt_callback = ModelCheckpoint(monitor='val_loss', save_top_k=1)
         callbacks = [
@@ -92,7 +97,7 @@ def mask_search(args):
 
         # constructing a new trainer resets optimizers and schedulers
         trainer = Trainer(
-            default_root_dir=dirpath,
+            default_root_dir=dir_path,
             enable_progress_bar=False,
             callbacks=callbacks,
             gpus=1,
@@ -108,7 +113,7 @@ def mask_search(args):
 
         model.prune(args.prune_amount)
 
-    with open(os.path.join(basepath, 'meta_logs.json'), 'w') as fp:
+    with open(os.path.join(base_path, 'meta_logs.json'), 'w') as fp:
         json.dump(meta_logs, fp)
 
 
